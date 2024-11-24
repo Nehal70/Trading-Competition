a7761768611ad60872d146f60acaab45c7f24918
Braden Jones <bjones416@gatech.edu>
2024-11-24 14:33:08 -0500

    Update PnLWidget.js

---
+++ src/widgets/PnLWidget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PnLWidget from "./PnLWidget.css";
+import samplePnLData from "../SampleData/samplePnLData.json";
+import sampleStockWidgetData from "../SampleData/sampleStockWidgetData.json";
+
+
+const PnLDashBoard = ({ ticker }) => {
+
+
+
+
+}
