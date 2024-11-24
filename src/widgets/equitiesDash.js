6f1a81e17ab5acc55217bd38c7352372d6664450
Nehal70 <63651397+Nehal70@users.noreply.github.com>
2024-11-24 13:03:33 -0500

    Update equitiesDash.js

--- src/widgets/equitiesDash.js
+++ src/widgets/equitiesDash.js
@@ -3,18 +3,19 @@ import sampleStockWidgetData from "../SampleData/sampleStockWidgetData.json";
 import "../Dashboard/Dashboard.css";
 import StockWidget from "./StockWidget.js";
 
-
-
 const equitiesDash = () => {
     const allTickers = sampleStockWidgetData.map(stock => stock.ticker);
     
     return (
-        <div className="widget equities">
-          {allTickers.map(ticker=> (
-            <StockWidget ticker={ticker} />
+        <>
+          {allTickers.map(stock => (
+            <StockWidget ticker={stock.ticker} />
           ))}
-        </div>
+        </>
       );
 }
 
-export default equitiesDash;
+export default equitiesDash; 
+
+
+
