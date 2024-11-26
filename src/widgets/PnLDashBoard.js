307c2b368cecd289204a44aad22a4a712a5a404b
Braden Jones <bjones416@gatech.edu>
2024-11-26 14:55:41 -0500

    Delete src/widgets/PnLDashBoard.js
    Got rid of it bc not needed

--- src/widgets/PnLDashBoard.js
+++
@@ -1,5 +0,0 @@
-import React, { useState } from 'react'
-import samplePositionData from "../SampleData/samplePositionData.json";
-import "../Dashboard/Dashboard.css";
-import PnLWidget from "./PnLWidget.js";
-
