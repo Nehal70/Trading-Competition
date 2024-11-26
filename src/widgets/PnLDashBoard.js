afe1b19752680732f96aa3e5e724dca52d7cdda1
Braden Jones <bjones416@gatech.edu>
2024-11-26 14:54:38 -0500

    Update PnLDashBoard.js

--- src/widgets/PnLDashBoard.js
+++ src/widgets/PnLDashBoard.js
@@ -1,3 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react'
 import samplePositionData from "../SampleData/samplePositionData.json";
 import "../Dashboard/Dashboard.css";
+import PnLWidget from "./PnLWidget.js";
+
